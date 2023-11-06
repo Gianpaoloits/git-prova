41ac3a33981e7f6d1d7c09c40926f35b3e21edce
Gianpaoloits <gianpaolo.gabancho.its@gmail.com>
2023-11-06 17:14:24 +0100

    soluzione alla domanda come riportare modifiche tra branch

--- src/pages/pageGabancho.tsx
+++ src/pages/pageGabancho.tsx
@@ -6,6 +6,7 @@ const PageGabancho = () => {
         <p>Gianpaolo Gabancho Carrion</p>
         <LogoGabancho/>
         <p>ho fatto il merge da master all'altro branch e poi ho fatto il rebase di master</p>
+        <p>Per riportare le modifiche fatte su master sulla mia branch dedicata faccio un merge</p>
         </div>
     );
   };
